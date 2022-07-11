34d062c91449ef3a9d4f9cd2d54724131009d132
Nitin Pandey <npandey@deqode.com>
2022-07-11 19:24:38 +0530

    Create a login form

---
+++ ReactFolder/my-app/src/Components/forms/Login.jsx
@@ -0,0 +1,68 @@
+import React, { useState } from "react";
+import "../../css/login.css";
+import axios from "axios";
+
+function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const loginHandle = (e) => {
+    e.preventDefault();
+    axios
+      .post("http://localhost:8000/login", { email, password })
+      .then((res) => {
+        console.log("user logged in successfully");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  return (
+    <div style={{ height: "100%" }}>
+      <div className="loginForm">
+        <form action="">
+          <div id="credentials">
+            <label htmlFor="login" placeholder="Enter email" className="label">
+              Email
+            </label>
+            <input
+              type="text"
+              className="input"
+              value={email}
+              onChange={(e) => {
+                setEmail(e.target.value);
+              }}
+            />
+            <br />
+            <br />
+
+            <label
+              htmlFor="login"
+              placeholder="Enter passoword"
+              className="label"
+            >
+              Password{" "}
+            </label>
+
+            <input
+              type="text"
+              className="input"
+              value={password}
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
+            />
+            <br />
+            <br />
+          </div>
+
+          <button type="button" id="button" onClick={loginHandle}>
+            Login
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
